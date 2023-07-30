13683e9b855edcc7acc69fcf6f58c8e49cffa66f
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-07-30 16:23:25 +0530

    Added : addressbook

--- src/com/bl/AddressBookMain.java
+++ src/com/bl/AddressBookMain.java
@@ -1,8 +1,35 @@
 package com.bl;
-
+import java.util.*;
 public class AddressBookMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
+
+        AddressBook addressBook = new AddressBook();
+        Scanner sc = new Scanner(System.in);
+
+        int Number;
+
+        do {
+            System.out.println(" 1. Add Contact \n 2. Display contact ");
+            System.out.println("Enter the  Number: ");
+
+            Number = sc.nextInt();
+
+            switch (Number) {
+
+                case 1:
+                    addressBook.addContacts();
+                    break;
+                case 2:
+                    addressBook.displayContacts();
+                    break;
+                default:
+                    System.out.println(" Invalid Input!!! ");
+                    break;
+
+            }
+        } while (Number != 2);
     }
-}
+    }
+
